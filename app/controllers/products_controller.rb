160e40b97b2336a4ba1cab07253bd3788da50722
Mayuka Ogura <mayuka_ogura@wondershake.com>
2019-08-24 23:52:12 +0900

    ✨ Allow 3000 port access

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,28 @@
 class ProductsController < ApplicationController
 
-    def index
-        @product = Product.all
-        render json: @product
-    end
+  def index
+    @product = Product.all
+    render json: @product
+  end
 
-    def create
-        @product = Product.create(product: params[:product])
-        render json: @product
-    end
+  def create
+    @product = Product.create(product: params[:product])
+    render json: @product
+  end
 
-    def update
-        @product = Product.find(params[:id])
-        @product.update_attributes(product: params[:product])
-    end
+  def update
+    @product = Product.find(params[:id])
+    @product.update_attributes(product: params[:product])
+    render json: @product
+  end
 
-    def destory
-        @product = Product.find(params[:id])
-        if @product.destory
-            head :no_content, status: :ok
-        else
-            render json: @product.errors, status: :unprocessable_entity
-        end
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      head :no_content, status: :ok
+    else
+      render json: @product.errors, status: :unprocessable_entity
     end
+  end
+
 end
