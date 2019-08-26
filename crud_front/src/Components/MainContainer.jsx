64e09d1de4a9951ed8fbcc6201ebdc58eb5de6b6
rutko <t.222cm@gmail.com>
2019-08-26 10:46:34 +0900

    ✨ Communicate Rails API

---
+++ crud_front/src/Components/MainContainer.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import Axios from "axios";
+
+class MainContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            products: []
+        }
+    }
+
+componentDidMount() {
+    axios.get('http://localhost:3001/products')
+    .then ((results) => {
+        console.log(results)
+        this.setState({products: results.data})
+    })
+    .catch((data) => {
+        console.log(data)
+    })
+}
+    render() {
+        return (
+            <div class Name ='app-main'>
+            </div>
+        );
+    }
+
+}
+export default MainContainer
