6d41540e5f6927f08b06963dbe68648625dc56d8
rutko <t.222cm@gmail.com>
2019-08-31 23:43:38 +0900

    👍 Possible rendering

--- crud_front/src/App.js
+++ crud_front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainContainer/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-
     );
   }
 }
